5c1b14dfa00eb3a80141442a89126894c92be3a9
Nick Stricker <strickerdev@gmail.com>
2020-07-30 23:27:47 -0500

    html for nav changed, classes given

--- striper/src/components/Sidebar/Sidebar.js
+++ striper/src/components/Sidebar/Sidebar.js
@@ -7,11 +7,27 @@ function Sidebar() {
       <div className={`side-section side-head`}>
           <h2 className='side-header'>STRIPED</h2>
       </div>
+      <div className={`side-section side-nav`}>
+        <nav>
+          <div className='nav-section'>
+          <a href='#' className='active'>Latest Post</a>
+          </div>
+          <div className='nav-section'>
+            <a href='#'>Archives</a>
+          </div>
+          <div className='nav-section'> 
+            <a href='#'>About Me</a>
+          </div>
+          <div className='nav-section contacts'>
+            <a href='#'>Contact Me</a>
+          </div>
+          
+        </nav>
+      </div>
+      {/* <div className={`side-section`}></div>
       <div className={`side-section`}></div>
       <div className={`side-section`}></div>
-      <div className={`side-section`}></div>
-      <div className={`side-section`}></div>
-      <div className={`side-section`}></div>
+      <div className={`side-section`}></div> */}
     </div>
   );
 }
